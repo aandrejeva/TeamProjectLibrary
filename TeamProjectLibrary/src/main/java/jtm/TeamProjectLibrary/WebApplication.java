ccd7bc25828c67ba56f77f87432d332587a4b3c3
'Sintija-Git' <'sintija.jaspere@gmail.com'>
2020-07-08 15:54:04 +0300

    Added throws Exception for WebApplication + extra dependencies for pom.xml

--- TeamProjectLibrary/src/main/java/jtm/TeamProjectLibrary/WebApplication.java
+++ TeamProjectLibrary/src/main/java/jtm/TeamProjectLibrary/WebApplication.java
@@ -2,13 +2,16 @@ package jtm.TeamProjectLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class WebApplication {
 
-	public static void main(String [] args) {
+	public static void main(String [] args) throws Exception { 
 	SpringApplication.run(WebApplication.class, args);
 	
-	
+
 	}
 }
