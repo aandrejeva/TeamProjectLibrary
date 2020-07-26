7067db01174b7b352f4bb31033f11b6eac88eb3c
ArtjomsDenisenko <ArtjomsDenisenko@DESKTOP-LP1M2HE>
2020-07-26 20:25:35 +0300

    Mapping to proper fields added

--- TeamProjectLibrary/src/main/java/jtm/teamProjectLibrary/model/Customer.java
+++ TeamProjectLibrary/src/main/java/jtm/teamProjectLibrary/model/Customer.java
@@ -12,10 +12,10 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@Column(name = "first_name")
+	@Column(name = "firstName")
 	private String firstName;
 	
-	@Column(name = "last_name")
+	@Column(name = "lastName")
 	private String lastName;
 	
 	@Column(name = "birth_date")
